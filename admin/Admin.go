04cee1e2fe9d2749d1558cd08866b6444bdcc41c
Martel <martel.mosqueda@alumnos.udg.mx>
2020-12-01 21:59:00 -0600

    Ya es posible guardar los datos en el servidor :D

--- admin/Admin.go
+++ admin/Admin.go
@@ -1,6 +1,12 @@
 package admin
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	Util "../util"
+)
 
 type Data struct {
 	Subject map[string]map[string]float64
@@ -18,13 +24,11 @@ type Server struct {
 }
 
 // Inits the maps
-func (server *Server) Init(args *Args, reply *string) error {
+func (server *Server) Init() {
 	(*server).Maps.Student = make(map[string]map[string]float64)
 	(*server).Maps.Subject = make(map[string]map[string]float64)
 
-	*reply = "Successful creation of maps!"
-	fmt.Println(*reply)
-	return nil
+	fmt.Println("Successful creation of maps!")
 }
 
 // Checks if the student already has a grade for a subject,
@@ -40,7 +44,7 @@ func (server *Server) studentHasGrade(name string, subject string) bool {
 }
 
 // Adds a new stutent to the map thing
-func (server *Server) Add(args Args, reply *string) error {
+func (server *Server) Add(args Args) {
 	// create new student if not exist
 	if (*server).Maps.Student[args.Name] == nil {
 		(*server).Maps.Student[args.Name] = make(map[string]float64)
@@ -48,9 +52,8 @@ func (server *Server) Add(args Args, reply *string) error {
 
 	// check if grade is already saved
 	if (*server).studentHasGrade(args.Name, args.Subject) {
-		*reply = "Student already has a grade for that subject"
-		fmt.Println(*reply)
-		return nil
+		fmt.Println("Student already has a grade for that subject")
+		return
 	}
 	(*server).Maps.Student[args.Name][args.Subject] = args.Grade
 
@@ -60,52 +63,107 @@ func (server *Server) Add(args Args, reply *string) error {
 	}
 	(*server).Maps.Subject[args.Subject][args.Name] = args.Grade
 
-	*reply = "New student added!"
-	fmt.Println(*reply)
-	return nil
+	fmt.Println("New student added!")
 }
 
 // Returns the student average grade of all its subjects
-func (server *Server) StudentAverage(args Args, reply *float64) error {
-	*reply = 0
+func (server *Server) StudentAverage(args Args) float64 {
+	var result float64 = 0
 	fmt.Println("Client ask for student average")
 
 	for _, g := range (*server).Maps.Student[args.Name] {
-		*reply += g
+		result += g
 	}
 
-	*reply = *reply / float64(len((*server).Maps.Student[args.Name]))
-	return nil
+	return result / float64(len((*server).Maps.Student[args.Name]))
 }
 
 // Returns the subject average grade of all its students
-func (server *Server) SubjectAverage(args Args, reply *float64) error {
-	*reply = 0
+func (server *Server) SubjectAverage(args Args) float64 {
+	var result float64 = 0
 	fmt.Println("Client ask for subject average")
 
 	for _, g := range (*server).Maps.Subject[args.Subject] {
-		*reply += g
+		result += g
 	}
 
-	*reply = *reply / float64(len((*server).Maps.Subject[args.Subject]))
-
-	return nil
+	return result / float64(len((*server).Maps.Subject[args.Subject]))
 }
 
 // Returns the average of all students and subjects
-func (server *Server) GeneralAverage(args Args, reply *float64) error {
+func (server *Server) GeneralAverage(args Args) float64 {
 	var total float64
-	*reply = 0
+	var result float64 = 0
 	fmt.Println("Client ask for general average")
 
 	for _, subject := range (*server).Maps.Student {
 		for _, g := range subject {
-			*reply += g
+			result += g
 			total += 1
 		}
 	}
 
-	*reply = *reply / total
+	return result / total
+}
 
-	return nil
+// Default home page where the client can save the
+// students data
+func (server *Server) HomePage(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set(
+		"Content-Type",
+		"text/html",
+	)
+	// sending home.html
+	fmt.Fprintf(
+		res, Util.LoadHtml("home.html"),
+	)
+}
+
+// This page will show all the data stored by the client
+// on a single table
+func (server *Server) GradesPage(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set(
+		"Content-Type",
+		"text/html",
+	)
+	// sending grades.html
+	fmt.Fprintf(
+		res, Util.LoadHtml("grades.html"),
+	)
+}
+
+// this function will handle the requested methods from the
+// home form in order to save new data
+func (server *Server) Save(res http.ResponseWriter, req *http.Request) {
+	fmt.Println("SAVE method => " + req.Method)
+
+	switch req.Method {
+	case "POST":
+		if err := req.ParseForm(); err != nil {
+			fmt.Fprintf(res, "ParseForm() error %v", err)
+			return
+		}
+		(*server).SaveToMaps(req)
+		break
+	default:
+		fmt.Println("Cannot handle requested method :(")
+		break
+	}
+}
+
+// saves the req to the maps using the parsing the received method
+// to the args struct
+func (server *Server) SaveToMaps(req *http.Request) {
+	// getting grade
+	g, _ := strconv.ParseFloat(req.FormValue("grade"), 64)
+
+	// new args struct
+	args := Args{
+		Name:    req.FormValue("name"),
+		Grade:   g,
+		Subject: req.FormValue("subject"),
+	}
+
+	// add to the list
+	server.Add(args)
 }
--- server.go
+++ server.go
@@ -4,65 +4,28 @@ import (
 	"fmt"
 	"net/http"
 
+	Admin "./admin"
 	Util "./util"
 )
 
-// Default home page where the client can save the
-// students data
-func HomePage(res http.ResponseWriter, req *http.Request) {
-	res.Header().Set(
-		"Content-Type",
-		"text/html",
-	)
-	// sending home.html
-	fmt.Fprintf(
-		res, Util.LoadHtml("home.html"),
-	)
-}
-
-// This page will show all the data stored by the client
-// on a single table
-func GradesPage(res http.ResponseWriter, req *http.Request) {
-	res.Header().Set(
-		"Content-Type",
-		"text/html",
-	)
-	// sending grades.html
-	fmt.Fprintf(
-		res, Util.LoadHtml("grades.html"),
-	)
-}
-
-// this function will handle the requested methods from the
-// home form in order to save new data
-func Save(res http.ResponseWriter, req *http.Request) {
-	fmt.Println("SAVE method =>" + req.Method)
-	switch req.Method {
-	case "POST":
-		if err := req.ParseForm(); err != nil {
-			fmt.Fprintf(res, "ParseForm() error %v", err)
-			return
-		}
-		fmt.Println(req.PostForm)
-		break
-	default:
-		fmt.Println("Cannot handle requested method :(")
-		break
-	}
-}
-
 func main() {
+	// new server
+	server := Admin.Server{}
+	server.Init()
+
 	// handle styles
 	http.Handle("/", http.FileServer(http.Dir("./style")))
 
 	// home page
-	http.HandleFunc("/home", HomePage)
-	http.HandleFunc("/grades", GradesPage)
-	http.HandleFunc("/save", Save)
+	http.HandleFunc("/home", server.HomePage)
+	http.HandleFunc("/grades", server.GradesPage)
+	http.HandleFunc("/save", server.Save)
 
 	// debug purposes :D
 	fmt.Println("----------------------------------")
 	fmt.Println("STARTING SERVER...")
+
+	// start server
 	http.ListenAndServe(":9000", nil)
 	Util.ScanString()
 }
